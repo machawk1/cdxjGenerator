21c955c754701d1257def8aed7d10a356b63f536
Sawood Alam <ibnesayeed@gmail.com>
2020-07-15 15:48:50 -0400

    Expand date range and move a constant out of the loop

--- cdxjGenerator.py
+++ cdxjGenerator.py
@@ -19,6 +19,9 @@ def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
 
 def line_generator(provided_urir=None):
     fake = Faker()
+    start_date = datetime.date(year=1, month=1, day=1)
+    end_date = datetime.date(year=9999, month=12, day=31)
+    ipfs_char_range = string.ascii_letters + string.digits
     while True:
         urir = provided_urir or fake.uri()
 
@@ -26,8 +29,8 @@ def line_generator(provided_urir=None):
             urir,
             path_strip_trailing_slash_unless_empty=True)
 
-        date14 = fake.date_time().strftime('%Y%m%d%H%M%S')
-        ipfs_char_range = string.ascii_letters + string.digits
+        date14 = fake.date_time_between_dates(start_date,
+                end_date).strftime('%Y%m%d%H%M%S')
 
         locators = (f"urn:ipfs/{id_generator(46, ipfs_char_range)}/"
                     f"{id_generator(46, ipfs_char_range)}")
