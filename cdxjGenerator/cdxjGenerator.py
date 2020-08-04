f2c63fbc30f13bac0c8df9a699cecf8f35b40a47
Mat Kelly <me@matkelly.com>
2020-08-04 16:57:44 -0400

    Add main py for #3

--- cdxjGenerator/cdxjGenerator.py
+++ cdxjGenerator/cdxjGenerator.py
@@ -1,13 +1,3 @@
-"""
-cdxj TimeMap generator
-
-This is script to generate dummy CDXJ data for testing other tools.
-A sample of the expected output style is appended to this code.
-
-Run it with $ python3 cdxjGenerator.py [numberOfLines]
-"""
-__version__ = '0.0.1'
-
 import datetime
 from faker import Faker
 import random
---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from cdxjGenerator import __version__
+
+with open('README.md') as f:
+    long_description = f.read()
+desc = """cdxjGenerator"""
+
+setup(
+    name='cdxGenerator',
+    version=__version__,
+    url='https://github.com/machawk1/cdxjGenerator',
+    download_url="https://github.com/cdxjGenerator",
+    author='Mat Kelly',
+    author_email='me@matkelly.com',
+    description=desc,
+    packages=['cdxjGenerator'],
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    provides=[
+        'cdxjGenerator'
+    ],
+    install_requires=[
+        'Faker',
+        'surt>=0.3.0',
+        'tlds'
+    ],
+    entry_points="""
+        [console_scripts]
+        cdxjGenerator = cdxjGenerator.__main__:main
+    """,
+    zip_safe=False,
+    keywords='web archives replay cdxj memento',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Environment :: Web Environment',
+
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: System :: Archiving',
+        'Topic :: System :: Archiving :: Backup',
+        'Topic :: System :: Archiving :: Mirroring',
+        'Topic :: Utilities'
+    ]
+)
